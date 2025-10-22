1ec91a2da01136cfff0698b928bf8cacf4294a05
kimrazeltorrazo-sketch <kimrazeltorrazo@gmail.com>
2025-10-22 14:38:36 +0800

    Delete ParkingManagementSystem(NoCode)-20251020T000844Z-1-001/ParkingManagementSystem(NoCode) directory

--- ParkingManagementSystem(NoCode)-20251020T000844Z-1-001/ParkingManagementSystem(NoCode)/ParkingManagementSystem/ParkingManagementSystem/Modules/CurrentUser.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class CurrentUser
-    Public Property UserID As Integer
-    Public Property Username As String
-    Public Property FirstName As String
-    Public Property LastName As String
-    Public Property Role As String
-End Class
